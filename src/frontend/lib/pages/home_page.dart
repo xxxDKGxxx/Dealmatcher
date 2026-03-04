5bdb3e6fc313ae96b35232d8845a958e290546e9
pawel <szymanski.pawel.100@gmail.com>
2026-03-04 14:33:21 +0100

    init flutter + flutter CI

---
+++ src/frontend/lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DealMatcher'),
+        backgroundColor: theme.colorScheme.inversePrimary,
+      ),
+      body: ColoredBox(color: Colors.white),
+    );
+  }
+}
