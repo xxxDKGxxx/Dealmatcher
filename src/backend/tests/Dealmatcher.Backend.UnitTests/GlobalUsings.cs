04fbafe77b0b58348e768cbf969762cf78b6d91f
Fryderyk Wolny <wolny.fryderyk@gmail.com>
2026-03-25 11:20:18 +0100

    usuniecie usingu exampleagregate z testow

--- src/backend/tests/Dealmatcher.Backend.UnitTests/GlobalUsings.cs
+++ src/backend/tests/Dealmatcher.Backend.UnitTests/GlobalUsings.cs
@@ -6,4 +6,4 @@ global using NSubstitute;
 global using Serilog.Extensions.Logging;
 global using Shouldly;
 global using Xunit;
-global using ExampleEntity = Dealmatcher.Backend.Domain.EntityAggregates.ExampleAggregate.Example;
+//global using ExampleEntity = Dealmatcher.Backend.Domain.EntityAggregates.ExampleAggregate.Example;
