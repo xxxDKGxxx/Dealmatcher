20f699cfe69732717d7a0ec0be419f60c24a33c4
Fryderyk Wolny <wolny.fryderyk@gmail.com>
2026-03-28 16:46:45 +0100

    WIP: command i handler dla rejestracji

---
+++ src/backend/src/Dealmatcher.Backend.UseCases/Features/Users/Create/CreateUserCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Dealmatcher.Backend.UseCases.Features.Users.Create;
+
+public sealed class CreateUserCommandHandler(
+    IRepository<User> userRepository,
+    IMapper mapper,
+    IPasswordHasher passwordHasher) : ICommandHandler<CreateUserCommand, Result<UserDto>>
+{
+    public async Task<Result<UserDto>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var spec = new UserByEmailSpec(request.Email);
+        var existingUser = await userRepository.SingleOrDefaultAsync(spec, cancellationToken);
+
+        if (existingUser is not null)
+        {
+            return Result.Conflict("Email is already taken");
+        }
+
+        var passwordHash = passwordHasher.HashPassword(request.Password);
+
+        var newUser = new BasicUser(request.Email, passwordHash, request.Name, request.Surname);
+
+        await userRepository.AddAsync(newUser, cancellationToken);
+        await userRepository.SaveChangesAsync(cancellationToken);
+
+        var userDto = mapper.Map<UserDto>(newUser);
+        return Result.Success(userDto);
+    }
+}
