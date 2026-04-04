37b20822bbec0435084f9beebf2dc9dbf10e0b22
wolny <wolny.fryderyk@gmail.com>
2026-04-04 22:04:33 +0200

    Update user backend logic

---
+++ src/backend/src/Dealmatcher.Backend.UseCases/Features/Users/Update/UpdateUserCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Dealmatcher.Backend.UseCases.Features.Users.Update;
+public record UpdateUserCommand(
+    int UserId,
+    string Name,
+    string Surname) : ICommand<Result<UserDto>>;
---
+++ src/backend/src/Dealmatcher.Backend.UseCases/Features/Users/Update/UpdateUserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Dealmatcher.Backend.UseCases.Features.Users.Update;
+
+public class UpdateUserCommandHandler(
+    IRepository<User> userRepository,
+    IMapper mapper)
+    : ICommandHandler<UpdateUserCommand, Result<UserDto>>
+{
+    public async Task<Result<UserDto>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.GetByIdAsync(request.UserId, cancellationToken);
+
+        if (user is null)
+        {
+            return Result.NotFound();
+        }
+
+        user.UpdateName(request.Name);
+        user.UpdateSurname(request.Surname);
+
+        await userRepository.UpdateAsync(user, cancellationToken);
+        await userRepository.SaveChangesAsync(cancellationToken);
+
+        var userDto = mapper.Map<UserDto>(user);
+
+        return Result.Success(userDto);
+    }
+}
