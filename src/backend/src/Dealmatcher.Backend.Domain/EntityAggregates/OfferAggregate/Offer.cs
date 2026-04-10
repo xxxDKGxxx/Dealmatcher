50fb73d698ec0c57945c97bc513916d5e753aebd
janzaborski <janzaborski8@gmail.com>
2026-04-10 23:45:05 +0200

    fix non null description

--- src/backend/src/Dealmatcher.Backend.Domain/EntityAggregates/OfferAggregate/Offer.cs
+++ src/backend/src/Dealmatcher.Backend.Domain/EntityAggregates/OfferAggregate/Offer.cs
@@ -3,7 +3,7 @@
 public sealed class Offer : DealmatcherEntityBase, IAggregateRoot
 {
     public string Title { get; private set; } = null!;
-    public string? Description { get; private set; } = null!;
+    public string Description { get; private set; } = null!;
     public decimal Price { get; private set; }
     private readonly List<string> _images = [];
     public IReadOnlyCollection<string> Images => _images.AsReadOnly();
@@ -58,7 +58,10 @@ public sealed class Offer : DealmatcherEntityBase, IAggregateRoot
 
     public void UpdateDescription(string? description)
     {
-        Description = description;
+        if (!string.IsNullOrWhiteSpace(description))
+        {
+            Description = description;
+        }
     }
 
     public void UpdatePrice(decimal price)
---
+++ src/backend/src/Dealmatcher.Backend.Infrastructure/Data/Migrations/20260410214213_FixNonNullOfferDescription.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dealmatcher.Backend.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class FixNonNullOfferDescription : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "Description",
+            table: "Offers",
+            type: "nvarchar(400)",
+            maxLength: 400,
+            nullable: false,
+            defaultValue: "",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(400)",
+            oldMaxLength: 400,
+            oldNullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "Description",
+            table: "Offers",
+            type: "nvarchar(400)",
+            maxLength: 400,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "nvarchar(400)",
+            oldMaxLength: 400);
+    }
+}
