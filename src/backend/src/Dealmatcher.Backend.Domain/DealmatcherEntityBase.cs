9c5c8cbe4142ba77d0c11d43970f8ed3989ef8a4
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-04-18 18:37:09 +0200

    wip added IsDeleted field before migration

--- src/backend/src/Dealmatcher.Backend.Domain/DealmatcherEntityBase.cs
+++ src/backend/src/Dealmatcher.Backend.Domain/DealmatcherEntityBase.cs
@@ -4,8 +4,17 @@ public abstract class DealmatcherEntityBase : EntityBase
 {
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; private set; }
+    public bool IsDeleted { get; private set; } = false;
 
-    public abstract void Delete();
+    public virtual void Delete()
+    {
+        IsDeleted = false;
+    }
+
+    public virtual void UnDelete()
+    {
+        IsDeleted = true;
+    }
 
     public void MarkUpdated()
     {
