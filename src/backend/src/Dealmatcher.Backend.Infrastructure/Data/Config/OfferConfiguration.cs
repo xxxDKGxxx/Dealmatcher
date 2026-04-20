14cfd20f135d972d4ee68c23f92fbdc2abaa5e9d
xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>
2026-04-20 22:25:09 +0200

    Offer details retrieval endpoint (#70)
    * Endpoint implementation + tests (to be fixed)
    
    * Test fixes and correct Smart Enum queryfilter handling
    
    * Dotnet format
    
    * Namespace conflict fix

--- src/backend/src/Dealmatcher.Backend.Infrastructure/Data/Config/OfferConfiguration.cs
+++ src/backend/src/Dealmatcher.Backend.Infrastructure/Data/Config/OfferConfiguration.cs
@@ -8,54 +8,54 @@ public sealed class OfferConfiguration : DealmatcherBaseEntityConfiguration<Offe
 
         builder.ToTable($"{nameof(Offer)}s");
 
-        builder.HasQueryFilter(o => o.Seller.Status == UserStatus.Active);
+        builder.HasQueryFilter(o => o.Seller.Status == UserStatus.Active.Value);
 
-        builder.Property(o => o.Title)
-            .IsRequired()
-            .HasMaxLength(DataSchemaConstants.OfferTitleMaxLength);
+        builder
+          .Property(o => o.Title)
+          .IsRequired()
+          .HasMaxLength(DataSchemaConstants.OfferTitleMaxLength);
 
-        builder.Property(o => o.Description)
-            .IsRequired()
-            .HasMaxLength(DataSchemaConstants.OfferDescriptionMaxLength);
+        builder
+          .Property(o => o.Description)
+          .IsRequired()
+          .HasMaxLength(DataSchemaConstants.OfferDescriptionMaxLength);
 
-        builder.Property(o => o.Price)
-            .HasPrecision(18, 2)
-            .IsRequired();
+        builder.Property(o => o.Price).HasPrecision(18, 2).IsRequired();
 
-        builder.Property(o => o.Availability)
-            .IsRequired();
+        builder.Property(o => o.Availability).IsRequired();
 
-        builder.Property(o => o.Status)
-            .HasConversion(
-                s => s.Value,
-                v => OfferStatus.FromValue(v))
-            .IsRequired()
-            .HasMaxLength(DataSchemaConstants.StatusMaxLength);
+        builder
+          .Property(o => o.Status)
+          .HasConversion(s => s.Value, v => OfferStatus.FromValue(v))
+          .IsRequired()
+          .HasMaxLength(DataSchemaConstants.StatusMaxLength);
 
-        builder.HasOne(o => o.Seller)
-            .WithMany()
-            .HasForeignKey("SellerId")
-            .OnDelete(DeleteBehavior.NoAction);
+        builder
+          .HasOne(o => o.Seller)
+          .WithMany()
+          .HasForeignKey("SellerId")
+          .OnDelete(DeleteBehavior.NoAction);
 
-        builder.Navigation(o => o.Seller)
-            .AutoInclude();
+        builder.Navigation(o => o.Seller).AutoInclude();
 
-        builder.HasOne(o => o.Category)
-            .WithMany()
-            .HasForeignKey("CategoryId")
-            .OnDelete(DeleteBehavior.NoAction);
+        builder
+          .HasOne(o => o.Category)
+          .WithMany()
+          .HasForeignKey("CategoryId")
+          .OnDelete(DeleteBehavior.NoAction);
 
-        builder.Navigation(o => o.Category)
-            .AutoInclude();
+        builder.Navigation(o => o.Category).AutoInclude();
 
-        builder.HasMany(o => o.Properties)
-            .WithOne()
-            .HasForeignKey("OfferId")
-            .IsRequired()
-            .OnDelete(DeleteBehavior.NoAction);
+        builder
+          .HasMany(o => o.Properties)
+          .WithOne()
+          .HasForeignKey("OfferId")
+          .IsRequired()
+          .OnDelete(DeleteBehavior.NoAction);
 
-        builder.Navigation(o => o.Properties)
-            .UsePropertyAccessMode(PropertyAccessMode.Field)
-            .AutoInclude();
+        builder
+          .Navigation(o => o.Properties)
+          .UsePropertyAccessMode(PropertyAccessMode.Field)
+          .AutoInclude();
     }
 }
