be23ba29aca8862a38da583c78a0786592465069
janzaborski <janzaborski8@gmail.com>
2026-04-22 00:16:13 +0200

    OfferSearchSpecificationBase read parameters fix

--- src/backend/src/Dealmatcher.Backend.Domain/Interfaces/OfferSuggestion/OfferSearchSpecificationBase.cs
+++ src/backend/src/Dealmatcher.Backend.Domain/Interfaces/OfferSuggestion/OfferSearchSpecificationBase.cs
@@ -1,6 +1,10 @@
 ﻿namespace Dealmatcher.Backend.Domain.Interfaces.OfferSuggestion;
 
 // TODO: filtrowanie po podstawowych parametrach
-public sealed class OfferSearchSpecificationBase(OfferSearchParameters parameters) : Specification<Offer>
+public sealed class OfferSearchSpecificationBase : Specification<Offer>
 {
+    public OfferSearchSpecificationBase(OfferSearchParameters parameters)
+    {
+        Query.Take(parameters.Limit);
+    }
 }
