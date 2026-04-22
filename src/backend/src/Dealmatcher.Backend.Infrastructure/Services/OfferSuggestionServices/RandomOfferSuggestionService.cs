a39145927e503a9870d257a94b27bb2246b97d0e
janzaborski <janzaborski8@gmail.com>
2026-04-22 23:56:52 +0200

    Basic... -> Random..., small fixes

---
+++ src/backend/src/Dealmatcher.Backend.Infrastructure/Services/OfferSuggestionServices/RandomOfferSuggestionService.cs
@@ -0,0 +1,9 @@
+﻿namespace Dealmatcher.Backend.Infrastructure.Services.OfferSuggestionServices;
+
+public sealed class RandomOfferSuggestionService : IOfferSuggestionService
+{
+    public Task<IEnumerable<Offer>> SuggestOffers(IEnumerable<Offer> offers, int limit, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(offers.OrderBy(_ => new Guid()).Take(limit));
+    }
+}
