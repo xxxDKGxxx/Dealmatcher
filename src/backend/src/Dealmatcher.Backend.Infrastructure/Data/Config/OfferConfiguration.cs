65560889293400262168b96cbd82720ead1bb38a
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-04-22 16:43:28 +0200

    Added image and tag handling

--- src/backend/src/Dealmatcher.Backend.Infrastructure/Data/Config/OfferConfiguration.cs
+++ src/backend/src/Dealmatcher.Backend.Infrastructure/Data/Config/OfferConfiguration.cs
@@ -2,60 +2,74 @@
 
 public sealed class OfferConfiguration : DealmatcherBaseEntityConfiguration<Offer>
 {
-    public override void Configure(EntityTypeBuilder<Offer> builder)
-    {
-        base.Configure(builder);
+  public override void Configure(EntityTypeBuilder<Offer> builder)
+  {
+    base.Configure(builder);
 
-        builder.ToTable($"{nameof(Offer)}s");
+    builder.ToTable($"{nameof(Offer)}s");
 
-        builder.HasQueryFilter(o => o.Seller.Status == UserStatus.Active.Value);
+    builder.HasQueryFilter(o => o.Seller.Status == UserStatus.Active.Value);
 
-        builder
-          .Property(o => o.Title)
-          .IsRequired()
-          .HasMaxLength(DataSchemaConstants.OfferTitleMaxLength);
+    builder
+      .Property(o => o.Title)
+      .IsRequired()
+      .HasMaxLength(DataSchemaConstants.OfferTitleMaxLength);
 
-        builder
-          .Property(o => o.Description)
-          .IsRequired()
-          .HasMaxLength(DataSchemaConstants.OfferDescriptionMaxLength);
+    builder
+      .Property(o => o.Description)
+      .IsRequired()
+      .HasMaxLength(DataSchemaConstants.OfferDescriptionMaxLength);
 
-        builder.Property(o => o.Price).HasPrecision(18, 2).IsRequired();
+    builder.Property(o => o.Price).HasPrecision(18, 2).IsRequired();
 
-        builder.Property(o => o.Availability).IsRequired();
+    builder.Property(o => o.Availability).IsRequired();
 
-        builder
-          .Property(o => o.Status)
-          .HasConversion(s => s.Value, v => OfferStatus.FromValue(v))
-          .IsRequired()
-          .HasMaxLength(DataSchemaConstants.StatusMaxLength);
+    builder
+      .Property(o => o.Images)
+      .HasField("_images")
+      .UsePropertyAccessMode(PropertyAccessMode.Field);
 
-        builder
-          .HasOne(o => o.Seller)
-          .WithMany()
-          .HasForeignKey("SellerId")
-          .OnDelete(DeleteBehavior.NoAction);
+    builder.Property(o => o.Tags).HasField("_tags").UsePropertyAccessMode(PropertyAccessMode.Field);
 
-        builder.Navigation(o => o.Seller).AutoInclude();
+    builder
+      .Property(o => o.Status)
+      .HasConversion(s => s.Value, v => OfferStatus.FromValue(v))
+      .IsRequired()
+      .HasMaxLength(DataSchemaConstants.StatusMaxLength);
 
-        builder
-          .HasOne(o => o.Category)
-          .WithMany()
-          .HasForeignKey("CategoryId")
-          .OnDelete(DeleteBehavior.NoAction);
+    builder
+      .Property(o => o.Images)
+      .HasField("_images")
+      .UsePropertyAccessMode(PropertyAccessMode.Field);
 
-        builder.Navigation(o => o.Category).AutoInclude();
+    builder.Property(o => o.Tags).HasField("_tags").UsePropertyAccessMode(PropertyAccessMode.Field);
 
-        builder
-          .HasMany(o => o.Properties)
-          .WithOne()
-          .HasForeignKey("OfferId")
-          .IsRequired()
-          .OnDelete(DeleteBehavior.NoAction);
+    builder
+      .HasOne(o => o.Seller)
+      .WithMany()
+      .HasForeignKey("SellerId")
+      .OnDelete(DeleteBehavior.NoAction);
 
-        builder
-          .Navigation(o => o.Properties)
-          .UsePropertyAccessMode(PropertyAccessMode.Field)
-          .AutoInclude();
-    }
+    builder.Navigation(o => o.Seller).AutoInclude();
+
+    builder
+      .HasOne(o => o.Category)
+      .WithMany()
+      .HasForeignKey("CategoryId")
+      .OnDelete(DeleteBehavior.NoAction);
+
+    builder.Navigation(o => o.Category).AutoInclude();
+
+    builder
+      .HasMany(o => o.Properties)
+      .WithOne()
+      .HasForeignKey("OfferId")
+      .IsRequired()
+      .OnDelete(DeleteBehavior.NoAction);
+
+    builder
+      .Navigation(o => o.Properties)
+      .UsePropertyAccessMode(PropertyAccessMode.Field)
+      .AutoInclude();
+  }
 }
