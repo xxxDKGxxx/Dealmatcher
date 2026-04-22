93d66786d00c0210618336e1d7697132e7132436
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-04-22 16:44:10 +0200

    Dotnet format

--- src/backend/src/Dealmatcher.Backend.Infrastructure/Data/Migrations/20260422132430_ImagesAndTags.cs
+++ src/backend/src/Dealmatcher.Backend.Infrastructure/Data/Migrations/20260422132430_ImagesAndTags.cs
@@ -2,39 +2,38 @@
 
 #nullable disable
 
-namespace Dealmatcher.Backend.Infrastructure.Data.Migrations
+namespace Dealmatcher.Backend.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class ImagesAndTags : Migration
 {
     /// <inheritdoc />
-    public partial class ImagesAndTags : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "Images",
-                table: "Offers",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "[]");
+        migrationBuilder.AddColumn<string>(
+            name: "Images",
+            table: "Offers",
+            type: "nvarchar(max)",
+            nullable: false,
+            defaultValue: "[]");
 
-            migrationBuilder.AddColumn<string>(
-                name: "Tags",
-                table: "Offers",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "[]");
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "Tags",
+            table: "Offers",
+            type: "nvarchar(max)",
+            nullable: false,
+            defaultValue: "[]");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Images",
-                table: "Offers");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Images",
+            table: "Offers");
 
-            migrationBuilder.DropColumn(
-                name: "Tags",
-                table: "Offers");
-        }
+        migrationBuilder.DropColumn(
+            name: "Tags",
+            table: "Offers");
     }
 }
