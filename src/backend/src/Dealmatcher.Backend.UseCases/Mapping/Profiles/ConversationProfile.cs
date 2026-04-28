be065568ae067a605f9a4839981850d9e7cbc61e
xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>
2026-04-28 10:14:29 +0200

    Conversation model (#88)
    * Conversation model
    
    * Auto mapper config for dtso
    
    * Changed unread count meaning
    
    * Moved local using to GlobalUsings.cs
    
    * Added Id to dtos

---
+++ src/backend/src/Dealmatcher.Backend.UseCases/Mapping/Profiles/ConversationProfile.cs
@@ -0,0 +1,56 @@
+﻿namespace Dealmatcher.Backend.UseCases.Mapping.Profiles;
+
+public class ConversationProfile : Profile
+{
+    public ConversationProfile()
+    {
+        CreateMap<Conversation, ConversationDto>()
+          .ForCtorParam(
+            nameof(ConversationDto.LastMessage),
+            opt => opt.MapFrom(c => c.LastMessage.Content)
+          )
+          .ForCtorParam(
+            nameof(ConversationDto.LastMessageAt),
+            opt => opt.MapFrom(c => c.LastMessage.CreatedAt)
+          )
+          .ForCtorParam(nameof(ConversationDto.Status), opt => opt.MapFrom(c => c.Status.Name))
+          .ForCtorParam(
+            nameof(ConversationDto.UnreadCount),
+            opt =>
+              opt.MapFrom(
+                (src, context) =>
+                {
+                    if (context.Items.TryGetValue("readerId", out var readerId))
+                    {
+                        return src.UnreadCount((int)readerId);
+                    }
+                    return -1;
+                }
+              )
+          );
+        CreateMap<Conversation, ConversationDetailDto>()
+          .ForCtorParam(
+            nameof(ConversationDetailDto.LastMessage),
+            opt => opt.MapFrom(c => c.LastMessage.Content)
+          )
+          .ForCtorParam(
+            nameof(ConversationDetailDto.LastMessageAt),
+            opt => opt.MapFrom(c => c.LastMessage.CreatedAt)
+          )
+          .ForCtorParam(nameof(ConversationDetailDto.Status), opt => opt.MapFrom(c => c.Status.Name))
+          .ForCtorParam(
+            nameof(ConversationDetailDto.UnreadCount),
+            opt =>
+              opt.MapFrom(
+                (src, context) =>
+                {
+                    if (context.Items.TryGetValue("readerId", out var readerId))
+                    {
+                        return src.UnreadCount((int)readerId);
+                    }
+                    return -1;
+                }
+              )
+          );
+    }
+}
---
+++ src/backend/src/Dealmatcher.Backend.UseCases/Mapping/Profiles/MessageProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace Dealmatcher.Backend.UseCases.Mapping.Profiles;
+
+public class MessageProfile : Profile
+{
+    public MessageProfile()
+    {
+        CreateMap<Message, MessageDto>()
+          .ForCtorParam(nameof(MessageDto.SenderId), opt => opt.MapFrom(m => m.Sender.Id));
+    }
+}
