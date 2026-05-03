0d88630426c1be3621681e7cdba4360ac186f357
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-05-03 15:59:54 +0200

    Working endpoint

--- src/backend/src/Dealmatcher.Backend.API/Extensions/ResultExtensions.cs
+++ src/backend/src/Dealmatcher.Backend.API/Extensions/ResultExtensions.cs
@@ -2,81 +2,106 @@
 
 public static class ResultExtensions
 {
-    public static async Task SendResult(this Result result, IEndpoint endpoint, CancellationToken ct = default)
+  public static async Task SendResult(
+    this Result result,
+    IEndpoint endpoint,
+    CancellationToken ct = default
+  )
+  {
+    var response = endpoint.HttpContext.Response;
+
+    if (result.IsSuccess)
     {
-        var response = endpoint.HttpContext.Response;
+      if (result.IsNoContent())
+      {
+        await response.SendNoContentAsync(ct);
+        return;
+      }
 
-        if (result.IsSuccess)
-        {
-            if (result.IsNoContent())
-            {
-                await response.SendNoContentAsync(ct);
-                return;
-            }
-            await response.SendOkAsync(cancellation: ct);
-            return;
-        }
+      if (result.IsCreated())
+      {
+        await Results.StatusCode(StatusCodes.Status201Created).ExecuteAsync(endpoint.HttpContext);
+        return;
+      }
 
-        await HandleErrors(result, response, ct);
+      await response.SendOkAsync(cancellation: ct);
+      return;
     }
 
-    public static async Task SendResult<T>(this Result<T> result, IEndpoint endpoint, CancellationToken ct = default)
+    await HandleErrors(result, response, ct);
+  }
+
+  public static async Task SendResult<T>(
+    this Result<T> result,
+    IEndpoint endpoint,
+    CancellationToken ct = default
+  )
+  {
+    var response = endpoint.HttpContext.Response;
+
+    if (result.IsSuccess)
     {
-        var response = endpoint.HttpContext.Response;
+      if (result.IsNoContent())
+      {
+        await response.SendNoContentAsync(ct);
+        return;
+      }
 
-        if (result.IsSuccess)
-        {
-            if (result.IsNoContent())
-            {
-                await response.SendNoContentAsync(ct);
-                return;
-            }
-            await response.SendAsync(result.Value, 200, cancellation: ct);
-            return;
-        }
-
-        await HandleErrors(result, response, ct);
+      if (result.IsCreated())
+      {
+        await response.SendAsync(result.Value, 201, cancellation: ct);
+        return;
+      }
+      await response.SendAsync(result.Value, 200, cancellation: ct);
+      return;
     }
 
-    private static async Task HandleErrors<T>(Result<T> result, HttpResponse response, CancellationToken ct)
+    await HandleErrors(result, response, ct);
+  }
+
+  private static async Task HandleErrors<T>(
+    Result<T> result,
+    HttpResponse response,
+    CancellationToken ct
+  )
+  {
+    switch (result.Status)
     {
-        switch (result.Status)
-        {
-            case ResultStatus.NotFound:
-                await response.SendNotFoundAsync(cancellation: ct);
-                break;
+      case ResultStatus.NotFound:
+        await response.SendNotFoundAsync(cancellation: ct);
+        break;
 
-            case ResultStatus.Unauthorized:
-                await response.SendUnauthorizedAsync(cancellation: ct);
-                break;
+      case ResultStatus.Unauthorized:
+        await response.SendUnauthorizedAsync(cancellation: ct);
+        break;
 
-            case ResultStatus.Forbidden:
-                await response.SendForbiddenAsync(cancellation: ct);
-                break;
+      case ResultStatus.Forbidden:
+        await response.SendForbiddenAsync(cancellation: ct);
+        break;
 
-            case ResultStatus.Invalid:
-                await response.SendAsync(result.ValidationErrors, 400, cancellation: ct);
-                break;
+      case ResultStatus.Invalid:
+        await response.SendAsync(result.ValidationErrors, 400, cancellation: ct);
+        break;
 
-            case ResultStatus.Conflict:
-                await response.SendAsync(result.Errors, 409, cancellation: ct);
-                break;
+      case ResultStatus.Conflict:
+        await response.SendAsync(result.Errors, 409, cancellation: ct);
+        break;
 
-            case ResultStatus.Error:
-                await response.SendAsync(result.Errors, 400, cancellation: ct);
-                break;
+      case ResultStatus.Error:
+        await response.SendAsync(result.Errors, 400, cancellation: ct);
+        break;
 
-            case ResultStatus.CriticalError:
-                await response.SendAsync(result.Errors, 500, cancellation: ct);
-                break;
+      case ResultStatus.CriticalError:
+        await response.SendAsync(result.Errors, 500, cancellation: ct);
+        break;
 
-            case ResultStatus.Unavailable:
-                await response.SendAsync(result.Errors, 503, cancellation: ct);
-                break;
+      case ResultStatus.Unavailable:
+        await response.SendAsync(result.Errors, 503, cancellation: ct);
+        break;
 
-            default:
-                await response.SendAsync(result.Errors, 500, cancellation: ct);
-                break;
-        }
+      default:
+        await response.SendAsync(result.Errors, 500, cancellation: ct);
+        break;
     }
+  }
 }
