c9d8ace5604698910bfc73fe81d20964ea4ec064
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-05-03 15:30:47 +0200

    Dotnet format

--- src/backend/src/Dealmatcher.Backend.API/Endpoints/Cart/Add/AddItem.cs
+++ src/backend/src/Dealmatcher.Backend.API/Endpoints/Cart/Add/AddItem.cs
@@ -3,47 +3,47 @@
 public class AddItem(IMediator mediator, IClaimsPrincipalManager claimsPrincipalManager)
   : Endpoint<AddItemRequest, CartItemDto>
 {
-  public override void Configure()
-  {
-    Version(1);
-    Post("/cart/items");
-    AllowAnonymous();
-
-    Description(d =>
-      d.Produces<CartItemDto>(201, "application/json")
-        .Produces(400)
-        .Produces(401)
-        .Produces(404)
-        .Produces(409)
-        .Produces(500)
-    );
-
-    Summary(s =>
+    public override void Configure()
     {
-      s.Summary = "Add item to cart";
-      s.Description = "Adds na offer to the user's cart";
-      s.Response<CategoryDto>(201, "Item added to cart successfully");
-      s.Response(400, "Invalid request");
-      s.Response(401, "Unauthorized");
-      s.Response(404, "Offer not found");
-      s.Response(409, "Item already in cart");
-      s.Response(500, "Internal server error");
-    });
-  }
+        Version(1);
+        Post("/cart/items");
+        AllowAnonymous();
 
-  public override async Task HandleAsync(AddItemRequest req, CancellationToken ct)
-  {
-    var requestingUserId = claimsPrincipalManager.GetUserId(User);
+        Description(d =>
+          d.Produces<CartItemDto>(201, "application/json")
+            .Produces(400)
+            .Produces(401)
+            .Produces(404)
+            .Produces(409)
+            .Produces(500)
+        );
 
-    if (requestingUserId is null)
-    {
-      await SendUnauthorizedAsync(ct);
-      return;
+        Summary(s =>
+        {
+            s.Summary = "Add item to cart";
+            s.Description = "Adds na offer to the user's cart";
+            s.Response<CategoryDto>(201, "Item added to cart successfully");
+            s.Response(400, "Invalid request");
+            s.Response(401, "Unauthorized");
+            s.Response(404, "Offer not found");
+            s.Response(409, "Item already in cart");
+            s.Response(500, "Internal server error");
+        });
     }
 
-    var request = new AddItemToCartCommand(requestingUserId.Value, req.OfferId, req.Quantity);
-    var result = await mediator.Send(request, ct);
+    public override async Task HandleAsync(AddItemRequest req, CancellationToken ct)
+    {
+        var requestingUserId = claimsPrincipalManager.GetUserId(User);
 
-    await result.SendResult(this, ct);
-  }
+        if (requestingUserId is null)
+        {
+            await SendUnauthorizedAsync(ct);
+            return;
+        }
+
+        var request = new AddItemToCartCommand(requestingUserId.Value, req.OfferId, req.Quantity);
+        var result = await mediator.Send(request, ct);
+
+        await result.SendResult(this, ct);
+    }
 }
