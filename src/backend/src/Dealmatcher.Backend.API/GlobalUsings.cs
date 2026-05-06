08030c9d16abb8cc354d757e0c24e77309368744
Jan Zaborski <142149301+janzaborski@users.noreply.github.com>
2026-05-06 11:17:44 +0200

    Feature/#156 pobieranie wartosci koszyka (#117)
    * Frontend cd fix
    
    * Fix tags tabulation
    
    * Get Cart Total Query and Handler
    
    * format
    
    * Get Cart Total Endpoint
    
    * format
    
    * stylistic changes
    
    ---------
    
    Co-authored-by: xxxDKGxxx <dominikzielinski678@gmail.com>
    Co-authored-by: xxxDKGxxx <163061447+xxxDKGxxx@users.noreply.github.com>

--- src/backend/src/Dealmatcher.Backend.API/GlobalUsings.cs
+++ src/backend/src/Dealmatcher.Backend.API/GlobalUsings.cs
@@ -23,6 +23,7 @@ global using Dealmatcher.Backend.Infrastructure.Data.Config;
 global using Dealmatcher.Backend.UseCases.Features.Authentication.Login;
 global using Dealmatcher.Backend.UseCases.Features.Cart.Add;
 global using Dealmatcher.Backend.UseCases.Features.Cart.Get;
+global using Dealmatcher.Backend.UseCases.Features.Cart.GetTotal;
 global using Dealmatcher.Backend.UseCases.Features.Cart.Update;
 global using Dealmatcher.Backend.UseCases.Features.Categories.Get;
 global using Dealmatcher.Backend.UseCases.Features.Conversations.Create;
