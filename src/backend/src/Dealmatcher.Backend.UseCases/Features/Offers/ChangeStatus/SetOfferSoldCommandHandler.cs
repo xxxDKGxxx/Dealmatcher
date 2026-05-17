5ee1fb824b2c2d7366b42308646a2d263f4db256
Jan Zaborski <142149301+janzaborski@users.noreply.github.com>
2026-05-17 18:28:26 +0200

    separate handlers for activating and setting sold status (#124)
    * separate handlers for activating and setting sold status
    
    * format

---
+++ src/backend/src/Dealmatcher.Backend.UseCases/Features/Offers/ChangeStatus/SetOfferSoldCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace Dealmatcher.Backend.UseCases.Features.Offers.ChangeStatus;
+
+public sealed class SetOfferSoldCommandHandler(
+    IReadRepository<User> usersRepository,
+    IRepository<Offer> offersRepository,
+    IMapper mapper) : ICommandHandler<SetOfferSoldCommand, Result<OfferDto>>
+{
+    public async Task<Result<OfferDto>> Handle(SetOfferSoldCommand request, CancellationToken cancellationToken)
+    {
+        var activeUserByIdSpec = new ActiveUserByIdSpec(request.userId);
+        var user = await usersRepository.FirstOrDefaultAsync(activeUserByIdSpec, cancellationToken);
+
+        if (user is null)
+        {
+            return Result.Unauthorized($"User with id: {request.userId} doesn't exist");
+        }
+
+        var offer = await offersRepository.GetByIdAsync(request.offerId, cancellationToken);
+
+        if (offer is null)
+        {
+            return Result.NotFound($"Offer with id: {request.offerId} doesn't exist");
+        }
+
+        if (user.Id != offer.Seller.Id)
+        {
+            return Result.Forbidden($"Offer with id: {request.offerId} doesn't belong to user with id: {request.userId}");
+        }
+
+        if (!offer.Status.CanBeSold)
+        {
+            return Result.Conflict($"Cannot set offer status to SOLD from status: {offer.Status}");
+        }
+
+        offer.Sell();
+        await offersRepository.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(mapper.Map<OfferDto>(offer));
+    }
+}
