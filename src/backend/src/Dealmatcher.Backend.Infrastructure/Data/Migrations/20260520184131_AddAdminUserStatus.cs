4919cc96e6b08ea4619d554af7ca6a450e5197a9
Jan Zaborski <142149301+janzaborski@users.noreply.github.com>
2026-05-21 01:01:28 +0200

    Feature/#203 status admina (#139)
    * Admin UserStatus
    
    * removed comments

---
+++ src/backend/src/Dealmatcher.Backend.Infrastructure/Data/Migrations/20260520184131_AddAdminUserStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dealmatcher.Backend.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class AddAdminUserStatus : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "IsPrivileged",
+            table: "Users");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "IsPrivileged",
+            table: "Users",
+            type: "bit",
+            nullable: false,
+            defaultValue: false);
+    }
+}
