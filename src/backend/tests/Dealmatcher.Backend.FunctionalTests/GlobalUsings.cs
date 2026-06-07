b4442bd581dbbb783e623e0e710d8268faee54de
janzaborski <janzaborski8@gmail.com>
2026-06-07 22:33:45 +0200

    small changes

--- src/backend/tests/Dealmatcher.Backend.FunctionalTests/GlobalUsings.cs
+++ src/backend/tests/Dealmatcher.Backend.FunctionalTests/GlobalUsings.cs
@@ -20,6 +20,7 @@ global using Dealmatcher.Backend.Domain.EntityAggregates.OfferAggregate.Dto;
 global using Dealmatcher.Backend.Domain.EntityAggregates.OfferAggregate.Properties;
 global using Dealmatcher.Backend.Domain.EntityAggregates.OfferAggregate.PropertyDefinitions;
 global using Dealmatcher.Backend.Domain.EntityAggregates.OfferAggregate.PropertyDefinitions.Dto;
+global using Dealmatcher.Backend.Domain.EntityAggregates.PurchaseAggregate;
 global using Dealmatcher.Backend.Domain.EntityAggregates.UserAggregate;
 global using Dealmatcher.Backend.Domain.EntityAggregates.UserAggregate.Dto;
 global using Dealmatcher.Backend.Domain.Interfaces;
