c9c2359839ef8c0915e1a7ffbf5f256f7c2c2ac2
Frycek1 <162837969+Frycek1@users.noreply.github.com>
2026-06-09 00:08:33 +0200

    Fix revokeBans spec (#174)
    * ban model
    
    * removed ids
    
    * migration
    
    * fix expiredBansSpec

--- src/backend/src/Dealmatcher.Backend.Domain/EntityAggregates/UserAggregate/Specifications/UsersWithExpiredBansSpec.cs
+++ src/backend/src/Dealmatcher.Backend.Domain/EntityAggregates/UserAggregate/Specifications/UsersWithExpiredBansSpec.cs
@@ -4,8 +4,9 @@ public sealed class UsersWithExpiredBansSpec : Specification<User>
 {
     public UsersWithExpiredBansSpec()
     {
-        Query.Where(u => u.Status == UserStatus.Banned &&
-                         u.Bans.Any(b => b.IsActive && b.ExpiresAt != null && b.ExpiresAt < DateTime.UtcNow))
+        var currentTime = DateTime.UtcNow;
+        Query
+            .Where(u => u.Bans.Any(b => b.IsActive && b.ExpiresAt.HasValue && b.ExpiresAt.Value < currentTime))
              .Include(u => u.Bans);
     }
 }
